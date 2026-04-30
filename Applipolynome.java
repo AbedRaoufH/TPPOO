31fdd1ef7231cfc745946e2606f4ad49ef86075e
Abderraouf <hamihamabedraouf@gmail.com>
2026-04-30 14:14:27 +0100

    Finished tp2

--- Applipolynome.java
+++ Applipolynome.java
@@ -1,5 +1,20 @@
 package TP02POO;
 
 public class Applipolynome {
-    
+    public static void main(String[] args) {
+         Polynome p1 = new Polynome() ; 
+        p1.setCoef1(1.0) ;
+        p1.setCoef2(1.0);
+         p1.setCoef3(1.0);
+         Polynome p2 = new Polynome(1, -2, -3) ; 
+         p1.afficher();
+         p2.afficher();
+         p1.evaluerPolynome(2);
+         Polynome p2d = p2.CalcDeriv() ; 
+          p2d.afficher();
+         p2d.evaluerPolynome(2);
+         p2.CalcDisc(); 
+         
+    }
+   
 }
